585e6f4720145e2cd26b4c49d581f28bfde1ec31
coolhound <coolhound36@gmail.com>
2022-05-26 15:49:20 +0100

    Make diagnostics disappear
    Use javascript querySelector function to set diagnostic display property to none

--- js/diagnostic.js
+++ js/diagnostic.js
@@ -1,27 +1,49 @@
 // Helper for viewport width diagnostics
 
-const sizeOutput = document.querySelector("#size");
-const heightOutput = document.querySelector("#height");
-const widthOutput = document.querySelector("#width");
+const outputStatus = false;
 
-function resizeListener() {
-    heightOutput.textContent = window.innerHeight;
-    widthOutput.textContent = window.innerWidth;
+const diagnosticContainer = document.querySelector("#diagnosticContainer");
+const diagnosticContent = document.querySelector("#diagnosticContent");
 
-    if ( window.innerWidth < 576 )
-    sizeOutput.textContent = "xs";
-    else if ( window.innerWidth < 768 )
-    sizeOutput.textContent = "sm";
-    else if ( window.innerWidth < 992 )
-    sizeOutput.textContent = "md";
-    else if ( window.innerWidth < 1200 )
-    sizeOutput.textContent = "lg";
-    else
-    sizeOutput.textContent = "xl";
+
+if (outputStatus == true)
+{
+/*
+    diagnosticContent.innerHTML = 
+          "<span class='navbar-text'>Size: <span id='size'>XXL</span></span>" +
+          "<span class='navbar-text'>Width: <span id='width'>9999</span></span>" +
+          "<span class='navbar-text'>Height: <span id='height'>9999</span></span>";
+*/
+    diagnosticContainer.style.display = "block";
+/*
+    const sizeOutput = document.querySelector("#size");
+    const heightOutput = document.querySelector("#height");
+    const widthOutput = document.querySelector("#width");
+
+    function resizeListener() {
+        heightOutput.textContent = window.innerHeight;
+        widthOutput.textContent = window.innerWidth;
+
+        if ( window.innerWidth < 576 )
+        sizeOutput.textContent = "xs";
+        else if ( window.innerWidth < 768 )
+        sizeOutput.textContent = "sm";
+        else if ( window.innerWidth < 992 )
+        sizeOutput.textContent = "md";
+        else if ( window.innerWidth < 1200 )
+        sizeOutput.textContent = "lg";
+        else
+        sizeOutput.textContent = "xl";
+    }
+
+    window.addEventListener("resize", resizeListener);
+
+    window.onload = function() {
+        resizeListener();
+    }
+*/
 }
-
-window.addEventListener("resize", resizeListener);
-
-window.onload = function() {
-    resizeListener();
+else
+{
+    diagnosticContainer.style.display = "none";
 }
